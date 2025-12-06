bacadfc8c73460ff9d9a3eabe4734808122537c3
byp <baoyipen@gmail.com>
2025-12-06 19:16:05 +0800

    Revert "11"
    This reverts commit 4796dd913f8bbf6ef056f8edf7aac9ed852d140b.

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        server: {
-            port: 3000,
-            host: '0.0.0.0',
-        },
-        plugins: [react()],
-        define: {
-            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '.'),
-            }
-        },
-        build: {
-            outDir: 'dist',       // 明确指定输出目录为 dist
-            emptyOutDir: true,    // 构建前清空 dist
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
         }
+      }
     };
 });
